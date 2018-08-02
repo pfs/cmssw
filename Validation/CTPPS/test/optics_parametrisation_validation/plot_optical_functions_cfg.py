99e70d38db90a73d1efd8ee6483d11efbb49bdb3
Jan Kaspar <jan.kaspar@cern.ch>
2018-08-02 11:40:14 +0200

    Added 2017 validation of optics parameterisation.

--- Validation/CTPPS/test/optics_parametrisation_validation/plot_optical_functions_cfg.py
+++ Validation/CTPPS/test/optics_parametrisation_validation/plot_optical_functions_cfg.py
@@ -19,6 +19,18 @@ process.maxEvents = cms.untracked.PSet(
 # retrieve the two plotters
 process.load('Validation.CTPPS.opticalFunctionsPlotter_cfi')
 
+from SimCTPPS.OpticsParameterisation.year_2017.lhcBeamConditions_cff import lhcBeamConditions
+
+# TODO: update
+process.ctppsPlotOpticalFunctions_45.opticsFile = cms.FileInPath("CondFormats/CTPPSOpticsObjects/data/2016_preTS2/version4-vale1/beam2/parametrization_6500GeV_0p4_185_reco.root")
+process.ctppsPlotOpticalFunctions_45.opticsObjects = cms.vstring("ip5_to_station_150_h_1_lhcb2", "ip5_to_station_150_h_2_lhcb2")
+process.ctppsPlotOpticalFunctions_45.beamConditions = lhcBeamConditions
+
+# TODO: update
+process.ctppsPlotOpticalFunctions_45.opticsFile = cms.FileInPath("CondFormats/CTPPSOpticsObjects/data/2016_preTS2/version4-vale1/beam1/parametrization_6500GeV_0p4_185_reco.root")
+process.ctppsPlotOpticalFunctions_45.opticsObjects = cms.vstring("ip5_to_station_150_h_1_lhcb1", "ip5_to_station_150_h_2_lhcb1")
+process.ctppsPlotOpticalFunctions_45.beamConditions = lhcBeamConditions
+
 # prepare the output file
 process.TFileService = cms.Service('TFileService',
     fileName = cms.string('optical_functions.root'),
