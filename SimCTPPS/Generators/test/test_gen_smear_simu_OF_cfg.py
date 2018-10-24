b51dfc87daf8ee731eccb8b39a9f90284f6b3055
Jan Kaspar <jan.kaspar@cern.ch>
2018-10-24 18:46:23 +0200

    Added config to test the PPXZ generator with the OF transport.

---
+++ SimCTPPS/Generators/test/test_gen_smear_simu_OF_cfg.py
@@ -0,0 +1,112 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+process = cms.Process('CTPPSFastSimulation', eras.ctpps_2016)
+
+# minimal logger settings
+process.MessageLogger = cms.Service("MessageLogger",
+    statistics = cms.untracked.vstring(),
+    destinations = cms.untracked.vstring('cerr'),
+    cerr = cms.untracked.PSet(
+        threshold = cms.untracked.string('WARNING')
+    )
+)
+
+# number of events
+process.source = cms.Source("EmptySource")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(400000)
+)
+
+# particle-data table
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+
+# particle generator
+process.load("SimCTPPS.Generators.PPXZGenerator_cfi")
+process.generator.verbosity = 0
+
+# random seeds
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+    sourceSeed = cms.PSet(initialSeed =cms.untracked.uint32(98765)),
+    generator = cms.PSet(initialSeed = cms.untracked.uint32(98766)),
+    SmearingGenerator = cms.PSet(initialSeed =cms.untracked.uint32(3849)),
+    beamDivergenceVtxGenerator = cms.PSet(initialSeed =cms.untracked.uint32(3849))
+)
+
+# geometry
+process.load("Geometry.VeryForwardGeometry.geometryRPFromDD_2017_cfi")
+del(process.XMLIdealGeometryESSource_CTPPS.geomXMLFiles[-1])
+process.XMLIdealGeometryESSource_CTPPS.geomXMLFiles.append("Validation/CTPPS/test_2017/RP_Dist_Beam_Cent.xml")
+
+# beam-smearing settings
+process.load("IOMC.EventVertexGenerators.beamDivergenceVtxGenerator_cfi")
+process.beamDivergenceVtxGenerator.src = cms.InputTag("generator", "unsmeared")
+
+process.beamDivergenceVtxGenerator.simulateBeamDivergence = True
+process.beamDivergenceVtxGenerator.simulateVertex = True
+
+# values in rad
+process.beamDivergenceVtxGenerator.beamDivergenceX = 20E-6
+process.beamDivergenceVtxGenerator.beamDivergenceY = 20E-6
+
+# values in cm
+process.beamDivergenceVtxGenerator.vertexMeanX = 0.
+process.beamDivergenceVtxGenerator.vertexMeanY = 0.
+process.beamDivergenceVtxGenerator.vertexMeanZ = 0.
+
+process.beamDivergenceVtxGenerator.vertexSigmaX = 10E-4
+process.beamDivergenceVtxGenerator.vertexSigmaY = 10E-4
+process.beamDivergenceVtxGenerator.vertexSigmaZ = 5
+
+# fast simulation
+process.load('SimCTPPS.OpticsParameterisation.year_2017_OF.ctppsFastProtonSimulation_cfi')
+process.ctppsFastProtonSimulation.verbosity = 0
+process.ctppsFastProtonSimulation.hepMCTag = cms.InputTag('beamDivergenceVtxGenerator')
+process.ctppsFastProtonSimulation.produceScoringPlaneHits = False
+process.ctppsFastProtonSimulation.produceRecHits = True
+process.ctppsFastProtonSimulation.checkApertures = False
+process.ctppsFastProtonSimulation.produceHitsRelativeToBeam = False
+process.ctppsFastProtonSimulation.roundToPitch = True
+
+# local track reco
+process.load('RecoCTPPS.TotemRPLocal.totemRPUVPatternFinder_cfi')
+process.totemRPUVPatternFinder.tagRecHit = cms.InputTag('ctppsFastProtonSimulation')
+
+process.load('RecoCTPPS.TotemRPLocal.totemRPLocalTrackFitter_cfi')
+
+process.load("RecoCTPPS.PixelLocal.ctppsPixelLocalTracks_cfi")
+process.ctppsPixelLocalTracks.label = "ctppsFastProtonSimulation"
+
+process.load('RecoCTPPS.TotemRPLocal.ctppsLocalTrackLiteProducer_cff')
+process.ctppsLocalTrackLiteProducer.includeDiamonds = False
+
+# XY distribution plotter
+process.ctppsTrackDistributionPlotter = cms.EDAnalyzer("CTPPSTrackDistributionPlotter",
+  tracksTag = cms.InputTag("ctppsLocalTrackLiteProducer"),
+  outputFile = cms.string("output_shape_smear.root")
+)
+
+# generator plots
+process.load("SimCTPPS.Generators.PPXZGeneratorValidation_cfi")
+process.ppxzGeneratorValidation.hepMCTag = cms.InputTag("generator", "unsmeared")
+process.ppxzGeneratorValidation.recoTracksTag = cms.InputTag("ctppsLocalTrackLiteProducer")
+process.ppxzGeneratorValidation.outputFile = "ppxzGeneratorValidation.root"
+
+# processing path
+process.p = cms.Path(
+    process.generator
+
+    * process.beamDivergenceVtxGenerator
+
+    * process.ctppsFastProtonSimulation
+
+    * process.totemRPUVPatternFinder
+    * process.totemRPLocalTrackFitter
+    * process.ctppsPixelLocalTracks
+    * process.ctppsLocalTrackLiteProducer
+
+    * process.ctppsTrackDistributionPlotter
+
+    * process.ppxzGeneratorValidation
+)
