8d7ec76ed9574a5ba23b194c367ab2bd46d025a2
Jan Kaspar <jan.kaspar@cern.ch>
2018-10-25 19:15:13 +0200

    Updated test configs.

--- RecoCTPPS/ProtonReconstruction/test/2017_OF/test_base_cff.py
+++ RecoCTPPS/ProtonReconstruction/test/2017_OF/test_base_cff.py
@@ -13,38 +13,56 @@ source = cms.Source("PoolSource",
   fileNames = cms.untracked.vstring()
 )
 
+# track distribution plotter
 ctppsTrackDistributionPlotter = cms.EDAnalyzer("CTPPSTrackDistributionPlotter",
     tracksTag = cms.InputTag("ctppsLocalTrackLiteProducer"),
     outputFile = cms.string("track_plots.root")
 )
 
-from SimCTPPS.OpticsParameterisation.2017_OF.ctppsDetectorPackages_cff import detectorPackages
-from SimCTPPS.OpticsParameterisation.2017_OF.lhcBeamConditions_cff import lhcBeamConditions
+# proton reconstruction
+from SimCTPPS.OpticsParameterisation.year_2017_OF.ctppsDetectorPackages_cff import detectorPackages
+from SimCTPPS.OpticsParameterisation.year_2017_OF.lhcBeamConditions_cff import lhcBeamConditions
 
-from RecoCTPPS.ProtonReconstruction.ctppsProtonReconstruction_cfi import *
-ctppsProtonReconstruction.beamConditions = lhcBeamConditions
-ctppsProtonReconstruction.detectorPackages = detectorPackages
+ctppsProtonReconstructionOF = cms.EDProducer('CTPPSProtonReconstructionOF',
+    verbosity = cms.untracked.uint32(0),
 
-ctppsProtonReconstruction.opticsFileBeam1 = cms.FileInPath('CondFormats/CTPPSOpticsObjects/data/2017/optical_functions_2017_140urad.root')
-ctppsProtonReconstruction.opticsFileBeam2 = cms.FileInPath('CondFormats/CTPPSOpticsObjects/data/2017/optical_functions_2017_140urad.root')
+    tagLocalTrackLite = cms.InputTag('ctppsLocalTrackLiteProducer'),
 
-ctppsProtonReconstruction.applyExperimentalAlignment = True
+    beamConditions = lhcBeamConditions,
+    detectorPackages = detectorPackages,
 
-ctppsProtonReconstruction.alignmentFiles = cms.vstring(
-    "RecoCTPPS/ProtonReconstruction/data/alignment/2017_preTS2/preliminary_alignment.out",
-    "RecoCTPPS/ProtonReconstruction/data/alignment/2017_postTS2/collect_alignments_2018_10_23.1.out"
+    opticsFileBeam1 = cms.FileInPath('CondFormats/CTPPSOpticsObjects/data/2017/optical_functions_2017_140urad.root'),
+    opticsFileBeam2 = cms.FileInPath('CondFormats/CTPPSOpticsObjects/data/2017/optical_functions_2017_140urad.root'),
+
+    applyExperimentalAlignment = cms.bool(True),
+    alignmentFiles = cms.vstring(
+      "RecoCTPPS/ProtonReconstruction/data/alignment/2017_preTS2/preliminary_alignment.out",
+      "RecoCTPPS/ProtonReconstruction/data/alignment/2017_postTS2/collect_alignments_2018_10_25.5.out"
+    ),
+
+    applyTestAlignment = cms.bool(False),
+    de_x_N = cms.double(0.),
+    de_x_F = cms.double(0.),
+    de_y_N = cms.double(0.),
+    de_y_F = cms.double(0.)
 )
 
-ctppsProtonReconstruction.verbosity = 0
-
+# reconstruction plotter
 ctppsProtonReconstructionPlotter = cms.EDAnalyzer("CTPPSProtonReconstructionPlotter",
     tagTracks = cms.InputTag("ctppsLocalTrackLiteProducer"),
-    tagRecoProtons = cms.InputTag("ctppsProtonReconstruction"),
+    tagRecoProtons = cms.InputTag("ctppsProtonReconstructionOF"),
+
+    rpId_45_F = cms.uint32(23),
+    rpId_45_N = cms.uint32(3),
+    rpId_56_N = cms.uint32(103),
+    rpId_56_F = cms.uint32(123),
+
     outputFile = cms.string("reco_plots.root")
 )
 
+# processing sequence
 p = cms.Path(
-    ctppsProtonReconstruction
+    ctppsProtonReconstructionOF
 
     * ctppsTrackDistributionPlotter
     * ctppsProtonReconstructionPlotter
@@ -55,29 +73,29 @@ import FWCore.ParameterSet.Types as CfgTypes
 
 # from 100
 
-def UseCrossingAngle100():
-  source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
-  myLumis = LumiList.LumiList(filename = "../../data/json/2017_postTS2/xangle_100.json").getCMSSWString().split(',')
-  source.lumisToProcess.extend(myLumis)
-
-def UseCrossingAngle110():
-  source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
-  myLumis = LumiList.LumiList(filename = "../../data/json/2017_postTS2/xangle_110.json").getCMSSWString().split(',')
-  source.lumisToProcess.extend(myLumis)
-
-def UseCrossingAngle120():
-  source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
-  myLumis = LumiList.LumiList(filename = "../../data/json/2017_preTS2/xangle_120.json").getCMSSWString().split(',')
-  source.lumisToProcess.extend(myLumis)
-  myLumis = LumiList.LumiList(filename = "../../data/json/2017_postTS2/xangle_120.json").getCMSSWString().split(',')
-  source.lumisToProcess.extend(myLumis)
-
-def UseCrossingAngle130():
-  source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
-  myLumis = LumiList.LumiList(filename = "../../data/json/2017_preTS2/xangle_130.json").getCMSSWString().split(',')
-  source.lumisToProcess.extend(myLumis)
-  myLumis = LumiList.LumiList(filename = "../../data/json/2017_postTS2/xangle_130.json").getCMSSWString().split(',')
-  source.lumisToProcess.extend(myLumis)
+#def UseCrossingAngle100():
+#  source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
+#  myLumis = LumiList.LumiList(filename = "../../data/json/2017_postTS2/xangle_100.json").getCMSSWString().split(',')
+#  source.lumisToProcess.extend(myLumis)
+#
+#def UseCrossingAngle110():
+#  source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
+#  myLumis = LumiList.LumiList(filename = "../../data/json/2017_postTS2/xangle_110.json").getCMSSWString().split(',')
+#  source.lumisToProcess.extend(myLumis)
+#
+#def UseCrossingAngle120():
+#  source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
+#  myLumis = LumiList.LumiList(filename = "../../data/json/2017_preTS2/xangle_120.json").getCMSSWString().split(',')
+#  source.lumisToProcess.extend(myLumis)
+#  myLumis = LumiList.LumiList(filename = "../../data/json/2017_postTS2/xangle_120.json").getCMSSWString().split(',')
+#  source.lumisToProcess.extend(myLumis)
+#
+#def UseCrossingAngle130():
+#  source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
+#  myLumis = LumiList.LumiList(filename = "../../data/json/2017_preTS2/xangle_130.json").getCMSSWString().split(',')
+#  source.lumisToProcess.extend(myLumis)
+#  myLumis = LumiList.LumiList(filename = "../../data/json/2017_postTS2/xangle_130.json").getCMSSWString().split(',')
+#  source.lumisToProcess.extend(myLumis)
 
 def UseCrossingAngle140():
   source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
@@ -86,10 +104,10 @@ def UseCrossingAngle140():
   myLumis = LumiList.LumiList(filename = "../../data/json/2017_postTS2/xangle_140.json").getCMSSWString().split(',')
   source.lumisToProcess.extend(myLumis)
 
-def UseCrossingAngle150():
-  source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
-  myLumis = LumiList.LumiList(filename = "../../data/json/2017_preTS2/xangle_150.json").getCMSSWString().split(',')
-  source.lumisToProcess.extend(myLumis)
-  myLumis = LumiList.LumiList(filename = "../../data/json/2017_postTS2/xangle_150.json").getCMSSWString().split(',')
-  source.lumisToProcess.extend(myLumis)
+#def UseCrossingAngle150():
+#  source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
+#  myLumis = LumiList.LumiList(filename = "../../data/json/2017_preTS2/xangle_150.json").getCMSSWString().split(',')
+#  source.lumisToProcess.extend(myLumis)
+#  myLumis = LumiList.LumiList(filename = "../../data/json/2017_postTS2/xangle_150.json").getCMSSWString().split(',')
+#  source.lumisToProcess.extend(myLumis)
 
