16433e8b60ad192eeae3004a3edb3c146536ae46
Jan Kaspar <jan.kaspar@cern.ch>
2018-10-31 18:30:12 +0100

    Added OFDB proton reconstruction module.

---
+++ RecoCTPPS/ProtonReconstruction/interface/ProtonReconstructionAlgorithmOFDB.h
@@ -0,0 +1,104 @@
+/****************************************************************************
+ *
+ * This is a part of CTPPS offline software.
+ * Authors:
+ *   Jan Kašpar
+ *   Laurent Forthomme
+ *
+ ****************************************************************************/
+
+#ifndef RecoCTPPS_ProtonReconstruction_ProtonReconstructionAlgorithmOFDB_h
+#define RecoCTPPS_ProtonReconstruction_ProtonReconstructionAlgorithmOFDB_h
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/CTPPSReco/interface/CTPPSLocalTrackLite.h"
+#include "DataFormats/ProtonReco/interface/ProtonTrack.h"
+
+#include "CondFormats/CTPPSOpticsObjects/interface/LHCOpticsApproximatorOF.h"
+
+#include "TFile.h"
+#include "TSpline.h"
+#include "Fit/Fitter.h"
+
+#include <map>
+#include <string>
+
+//----------------------------------------------------------------------------------------------------
+
+class ProtonReconstructionAlgorithmOFDB
+{
+  public:
+    ProtonReconstructionAlgorithmOFDB(double _xangle1, const std::string &optics_file_1, double _xangle2, const std::string &optics_file_2,
+      const edm::ParameterSet &beam_conditions, const std::vector<edm::ParameterSet> &detector_packages, unsigned int _verbosity);
+
+    ~ProtonReconstructionAlgorithmOFDB();
+
+    /// runs proton reconstruction using single-RP strategy
+    void reconstructFromSingleRP(const std::vector<const CTPPSLocalTrackLite*> &input, std::vector<reco::ProtonTrack> &output) const;
+
+    /// runs proton reconstruction using multiple-RP strategy
+    void reconstructFromMultiRP(const std::vector<const CTPPSLocalTrackLite*> &input, std::vector<reco::ProtonTrack> &output) const;
+
+    void Init(double xangle);
+
+    void Release();
+
+  private:
+    unsigned int verbosity;
+    bool initialized;
+
+    edm::ParameterSet beamConditions_;
+    double halfCrossingAngleSector45_, halfCrossingAngleSector56_;
+    double yOffsetSector45_, yOffsetSector56_;
+
+    double xangle1, xangle2;
+    std::string opticsFile1, opticsFile2;
+  
+    std::map<unsigned int, std::string> idNameMap;
+
+    /// optics data associated with 1 RP
+    struct RPOpticsData
+    {
+      LHCOpticsApproximatorOF *optics;
+      TSpline3 *s_xi_vs_x, *s_y0_vs_xi, *s_v_y_vs_xi, *s_L_y_vs_xi;
+      double x0, y0;  // beam position, m
+      double ch0, ch1;  // linear approximation (intercept and slope) of x(xi)
+      double la0, la1;  // linear approximation (intercept and slope) of L_x(xi)
+    };
+
+    /// map: RP id --> optics data
+    std::map<unsigned int, RPOpticsData> m_rp_optics_;
+
+    /// class for calculation of chi^2
+    class ChiSquareCalculator
+    {
+      public:
+        ChiSquareCalculator( const edm::ParameterSet& bc) :
+          beamConditions_( bc ),
+          halfCrossingAngleSector45_( bc.getParameter<double>( "halfCrossingAngleSector45" ) ),
+          halfCrossingAngleSector56_( bc.getParameter<double>( "halfCrossingAngleSector56" ) ),
+          yOffsetSector45_( bc.getParameter<double>( "yOffsetSector45" ) ),
+          yOffsetSector56_( bc.getParameter<double>( "yOffsetSector56" ) )
+        {
+        }
+
+        double operator() (const double *parameters) const;
+
+        const std::vector<const CTPPSLocalTrackLite*> *tracks;
+        const std::map<unsigned int, RPOpticsData> *m_rp_optics;
+
+      private:
+        edm::ParameterSet beamConditions_;
+        double halfCrossingAngleSector45_, halfCrossingAngleSector56_;
+        double yOffsetSector45_, yOffsetSector56_;
+    };
+
+    /// fitter object
+    std::unique_ptr<ROOT::Fit::Fitter> fitter_;
+
+    /// object to calculate chi^2
+    std::unique_ptr<ChiSquareCalculator> chiSquareCalculator_;
+};
+
+#endif
---
+++ RecoCTPPS/ProtonReconstruction/plugins/CTPPSProtonReconstructionOFDB.cc
@@ -0,0 +1,257 @@
+/****************************************************************************
+ *
+ * This is a part of CTPPS offline software.
+ * Authors:
+ *   Jan Kašpar
+ *   Laurent Forthomme
+ *
+ ****************************************************************************/
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/ESWatcher.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/CTPPSDetId/interface/CTPPSDetId.h"
+#include "DataFormats/CTPPSReco/interface/CTPPSLocalTrackLite.h"
+#include "DataFormats/ProtonReco/interface/ProtonTrack.h"
+
+#include "RecoCTPPS/ProtonReconstruction/interface/ProtonReconstructionAlgorithmOFDB.h"
+
+#include "CondFormats/RunInfo/interface/LHCInfo.h"
+#include "CondFormats/DataRecord/interface/LHCInfoRcd.h"
+
+#include "RecoCTPPS/ProtonReconstruction/interface/alignment_classes.h"
+#include "RecoCTPPS/ProtonReconstruction/interface/fill_info.h"
+
+//----------------------------------------------------------------------------------------------------
+
+class CTPPSProtonReconstructionOFDB : public edm::stream::EDProducer<>
+{
+  public:
+    explicit CTPPSProtonReconstructionOFDB(const edm::ParameterSet&);
+    ~CTPPSProtonReconstructionOFDB();
+
+    static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+  private:
+    virtual void produce(edm::Event&, const edm::EventSetup&) override;
+
+    edm::EDGetTokenT< std::vector<CTPPSLocalTrackLite> > tracksToken_;
+
+    unsigned int verbosity;
+
+    edm::ParameterSet beamConditions_;
+    std::vector<edm::ParameterSet> detectorPackages_;
+
+    double xangle1_, xangle2_;
+    edm::FileInPath opticsFile1_, opticsFile2_;
+
+    bool applyExperimentalAlignment;
+    std::vector<std::string> alignmentFiles_;
+
+    bool applyTestAlignment;
+    double de_x_N, de_x_F, de_y_N, de_y_F;
+
+    AlignmentResultsCollection alignmentCollection_;
+
+    ProtonReconstructionAlgorithmOFDB algorithm_;
+
+    edm::ESWatcher<LHCInfoRcd> lhcInfoWatcher;
+};
+
+//----------------------------------------------------------------------------------------------------
+
+using namespace std;
+using namespace edm;
+
+//----------------------------------------------------------------------------------------------------
+
+CTPPSProtonReconstructionOFDB::CTPPSProtonReconstructionOFDB( const edm::ParameterSet& iConfig ) :
+  tracksToken_( consumes< std::vector<CTPPSLocalTrackLite> >( iConfig.getParameter<edm::InputTag>( "tagLocalTrackLite" ) ) ),
+
+  verbosity                   ( iConfig.getUntrackedParameter<unsigned int>( "verbosity", 0 ) ),
+
+  beamConditions_             ( iConfig.getParameter<edm::ParameterSet>( "beamConditions" ) ),
+  detectorPackages_           ( iConfig.getParameter<std::vector<edm::ParameterSet>>( "detectorPackages" ) ),
+
+  xangle1_                    ( iConfig.getParameter<double>("xangle1") ),
+  xangle2_                    ( iConfig.getParameter<double>("xangle2") ),
+
+  opticsFile1_            ( iConfig.getParameter<edm::FileInPath>( "opticsFile1" ) ),
+  opticsFile2_            ( iConfig.getParameter<edm::FileInPath>( "opticsFile2" ) ),
+
+  applyExperimentalAlignment  ( iConfig.getParameter<bool>( "applyExperimentalAlignment" ) ),
+  alignmentFiles_              ( iConfig.getParameter<std::vector<std::string>>( "alignmentFiles" ) ),
+
+  applyTestAlignment  ( iConfig.getParameter<bool>( "applyTestAlignment" ) ),
+  de_x_N(iConfig.getParameter<double>("de_x_N")),
+  de_x_F(iConfig.getParameter<double>("de_x_F")),
+  de_y_N(iConfig.getParameter<double>("de_y_N")),
+  de_y_F(iConfig.getParameter<double>("de_y_F")),
+
+  algorithm_(xangle1_, opticsFile1_.fullPath(), xangle2_, opticsFile2_.fullPath(), beamConditions_, detectorPackages_, verbosity)
+{
+  produces<vector<reco::ProtonTrack>>();
+
+  //  load alignment collection
+  if (applyExperimentalAlignment)
+  {
+    for (const auto &f : alignmentFiles_)
+    {
+      FileInPath fip(f);
+      if (alignmentCollection_.Load(fip.fullPath()) != 0)
+        throw cms::Exception("CTPPSProtonReconstructionOFDB") << "Cannot load alignment from file '" << f << "'.";
+    }
+  }
+
+  // load fill-alignment mapping
+  InitFillInfoCollection();
+}
+
+//----------------------------------------------------------------------------------------------------
+
+void CTPPSProtonReconstructionOFDB::fillDescriptions(ConfigurationDescriptions& descriptions)
+{
+  ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+//----------------------------------------------------------------------------------------------------
+
+CTPPSProtonReconstructionOFDB::~CTPPSProtonReconstructionOFDB()
+{
+}
+
+//----------------------------------------------------------------------------------------------------
+
+void CTPPSProtonReconstructionOFDB::produce(Event& event, const EventSetup &eventSetup)
+{
+  // get conditions
+  edm::ESHandle<LHCInfo> pSetup;
+  const string label = "";
+  eventSetup.get<LHCInfoRcd>().get(label, pSetup);
+
+  if (lhcInfoWatcher.check(eventSetup))
+  {
+    const LHCInfo* pInfo = pSetup.product();
+    cout << event.id() << endl;
+    algorithm_.Init(pInfo->crossingAngle());
+  }
+
+  // get input
+  Handle< vector<CTPPSLocalTrackLite> > tracks;
+  event.getByToken(tracksToken_, tracks);
+
+  // prepare output
+  unique_ptr<vector<reco::ProtonTrack>> output( new vector<reco::ProtonTrack> );
+
+  // keep only tracks from tracker RPs
+  vector<CTPPSLocalTrackLite> tracksFiltered;
+  for (const auto &tr : *tracks)
+  {
+    CTPPSDetId rpId(tr.getRPId());
+    if (rpId.subdetId() == CTPPSDetId::sdTrackingStrip || rpId.subdetId() == CTPPSDetId::sdTrackingPixel)
+      tracksFiltered.push_back(tr);
+  }
+
+  // get and apply alignment
+  auto tracksAligned = tracksFiltered;
+
+  if (applyExperimentalAlignment)
+  {
+    FillInfo fillInfo;
+    unsigned int ret = fillInfoCollection.FindByRun(event.id().run(), fillInfo);
+    if (ret != 0)
+    {
+      printf("ERROR: cannot get fill info.\n");
+      event.put(move(output));
+      return;
+    }
+
+    const auto alignment_it = alignmentCollection_.find(fillInfo.alignmentTag);
+    if (alignment_it == alignmentCollection_.end())
+    {
+      printf("ERROR: cannot get alignment.\n");
+      event.put(move(output));
+      return;
+    }
+
+    tracksAligned = alignment_it->second.Apply(tracksFiltered);
+  }
+
+  if (applyTestAlignment)
+  {
+    tracksAligned.clear();
+    for (const auto &t : tracksFiltered)
+    {
+      CTPPSDetId rpId(t.getRPId());
+
+      double de_x = 0., de_y = 0.;
+      // TODO: this is not always correct
+      if (rpId.rp() == 2) { de_x = de_x_N; de_y = de_y_N; }
+      if (rpId.rp() == 3) { de_x = de_x_F; de_y = de_y_F; }
+
+      tracksAligned.emplace_back(t.getRPId(), t.getX() + de_x, t.getXUnc(), t.getY() + de_y, t.getYUnc());
+    }
+  }
+
+  if (verbosity)
+  {
+    printf("\n---------- %u:%llu ----------\n", event.id().run(), event.id().event());
+    for (const auto &tr : tracksAligned)
+    {
+      CTPPSDetId rpId(tr.getRPId());
+      unsigned int decRPId = rpId.arm()*100 + rpId.station()*10 + rpId.rp();
+      printf("%u (%u): x=%.3f, y=%.3f mm\n", tr.getRPId(), decRPId, tr.getX(), tr.getY());
+    }
+  }
+
+  // split input per sector
+  vector<const CTPPSLocalTrackLite*> tracks_45, tracks_56;
+  map<unsigned int, unsigned int> nTracksPerRP;
+  for (const auto &tr : tracksAligned)
+  {
+    CTPPSDetId rpId(tr.getRPId());
+    if (rpId.arm() == 0)
+      tracks_45.push_back(&tr);
+    else
+      tracks_56.push_back(&tr);
+
+    nTracksPerRP[tr.getRPId()]++;
+  }
+
+  // for the moment: check whether there is no more than 1 track in each arm
+  bool singleTrack_45 = true, singleTrack_56 = true;
+  for (const auto &p : nTracksPerRP)
+  {
+    if (p.second > 1)
+    {
+      CTPPSDetId rpId(p.first);
+      if (rpId.arm() == 0)
+        singleTrack_45 = false;
+      if (rpId.arm() == 1)
+        singleTrack_56 = false;
+    }
+  }
+
+  // run reconstruction per sector
+  algorithm_.reconstructFromSingleRP(tracks_45, *output);
+  algorithm_.reconstructFromSingleRP(tracks_56, *output);
+
+  if (singleTrack_45)
+    algorithm_.reconstructFromMultiRP(tracks_45, *output);
+  if (singleTrack_56)
+    algorithm_.reconstructFromMultiRP(tracks_56, *output);
+
+  // save output to event
+  event.put(move(output));
+}
+
+//----------------------------------------------------------------------------------------------------
+
+DEFINE_FWK_MODULE(CTPPSProtonReconstructionOFDB);
