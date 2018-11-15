f7cbefc9fd947192ae2daa14165073df63b65590
Jan Kaspar <jan.kaspar@cern.ch>
2018-11-15 13:03:40 +0100

    Added near-far correlation plots.

--- Validation/CTPPS/plugins/CTPPSTrackDistributionPlotter.cc
+++ Validation/CTPPS/plugins/CTPPSTrackDistributionPlotter.cc
@@ -22,6 +22,7 @@
 #include "TFile.h"
 #include "TH2D.h"
 #include "TProfile.h"
+#include "TProfile2D.h"
 
 #include <map>
 
@@ -76,6 +77,36 @@ class CTPPSTrackDistributionPlotter : public edm::one::EDAnalyzer<>
     };
 
     std::map<unsigned int, RPPlots> rpPlots;
+
+
+    struct ArmPlots
+    {
+      TProfile2D *p2_de_x_vs_x_y;
+      TProfile2D *p2_de_y_vs_x_y;
+
+      void init()
+      {
+        p2_de_x_vs_x_y = new TProfile2D("", ";x;y", 40, 0., 40., 40, -20., +20.);
+        p2_de_y_vs_x_y = new TProfile2D("", ";x;y", 40, 0., 40., 40, -20., +20.);
+      }
+
+      void fill(double x_N, double y_N, double x_F, double y_F)
+      {
+        if (p2_de_x_vs_x_y == NULL)
+          init();
+
+        p2_de_x_vs_x_y->Fill(x_N, y_N, x_F - x_N);
+        p2_de_y_vs_x_y->Fill(x_N, y_N, y_F - y_N);
+      }
+
+      void write() const
+      {
+        p2_de_x_vs_x_y->Write("p2_de_x_vs_x_y");
+        p2_de_y_vs_x_y->Write("p2_de_y_vs_x_y");
+      }
+    };
+
+    std::map<unsigned int, ArmPlots> armPlots;
 };
 
 //----------------------------------------------------------------------------------------------------
@@ -107,6 +138,22 @@ void CTPPSTrackDistributionPlotter::analyze( const edm::Event& iEvent, const edm
     unsigned int rpDecId = rpId.arm()*100 + rpId.station()*10 + rpId.rp();
     rpPlots[rpDecId].fill(trk.getX(), trk.getY());
   }
+
+  for (const auto& t1 : *tracks)
+  {
+    CTPPSDetId rpId1(t1.getRPId());
+
+    for (const auto& t2 : *tracks)
+    {
+      CTPPSDetId rpId2(t2.getRPId());
+
+      if (rpId1.arm() != rpId2.arm())
+        continue;
+
+      if (rpId1.station() == 0 && rpId2.station() == 2)
+        armPlots[rpId1.arm()].fill(t1.getX(), t1.getY(), t2.getX(), t2.getY());
+    }
+  }
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -124,6 +171,12 @@ void CTPPSTrackDistributionPlotter::endJob()
   for (const auto it : rpPlots)
   {
     gDirectory = f_out->mkdir(Form("RP %u", it.first));
+    it.second.write();
+  }
+
+  for (const auto it : armPlots)
+  {
+    gDirectory = f_out->mkdir(Form("arm %u", it.first));
     it.second.write();
   }
 }
