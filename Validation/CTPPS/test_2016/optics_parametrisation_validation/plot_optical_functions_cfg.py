9977b576a614865404dedfc613403a3c8ab178ff
Jan Kaspar <jan.kaspar@cern.ch>
2018-11-15 13:31:30 +0100

    Fixed config file.

--- Validation/CTPPS/test_2016/optics_parametrisation_validation/plot_optical_functions_cfg.py
+++ Validation/CTPPS/test_2016/optics_parametrisation_validation/plot_optical_functions_cfg.py
@@ -16,20 +16,23 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(0)
 )
 
-# retrieve the two plotters
-process.load('Validation.CTPPS.opticalFunctionsPlotter_cfi')
+from SimCTPPS.OpticsParameterisation.year_2016.lhcBeamConditions_cff import lhcBeamConditions
 
-from SimCTPPS.OpticsParameterisation.year_2017.lhcBeamConditions_cff import lhcBeamConditions
+# plotter for functions in sector 45
+process.ctppsPlotOpticalFunctions_45 = cms.EDAnalyzer("OpticalFunctionsPlotter",
+    opticsFile = cms.FileInPath("CondFormats/CTPPSOpticsObjects/data/2016_preTS2/version4-vale1/beam2/parametrization_6500GeV_0p4_185_reco.root"),
+    opticsObjects = cms.vstring("ip5_to_station_150_h_1_lhcb2", "ip5_to_station_150_h_2_lhcb2"),
+    beamConditions = lhcBeamConditions,
+    minXi = cms.double(0.0),
+    maxXi = cms.double(0.301),
+    xiStep = cms.double(0.001),
+)
 
-# TODO: update
-process.ctppsPlotOpticalFunctions_45.opticsFile = cms.FileInPath("CondFormats/CTPPSOpticsObjects/data/2016_preTS2/version4-vale1/beam2/parametrization_6500GeV_0p4_185_reco.root")
-process.ctppsPlotOpticalFunctions_45.opticsObjects = cms.vstring("ip5_to_station_150_h_1_lhcb2", "ip5_to_station_150_h_2_lhcb2")
-process.ctppsPlotOpticalFunctions_45.beamConditions = lhcBeamConditions
-
-# TODO: update
-process.ctppsPlotOpticalFunctions_45.opticsFile = cms.FileInPath("CondFormats/CTPPSOpticsObjects/data/2016_preTS2/version4-vale1/beam1/parametrization_6500GeV_0p4_185_reco.root")
-process.ctppsPlotOpticalFunctions_45.opticsObjects = cms.vstring("ip5_to_station_150_h_1_lhcb1", "ip5_to_station_150_h_2_lhcb1")
-process.ctppsPlotOpticalFunctions_45.beamConditions = lhcBeamConditions
+# plotter for functions in sector 56
+process.ctppsPlotOpticalFunctions_56 = process.ctppsPlotOpticalFunctions_45.clone(
+    opticsFile = cms.FileInPath("CondFormats/CTPPSOpticsObjects/data/2016_preTS2/version4-vale1/beam1/parametrization_6500GeV_0p4_185_reco.root"),
+    opticsObjects = cms.vstring("ip5_to_station_150_h_1_lhcb1", "ip5_to_station_150_h_2_lhcb1"),
+)
 
 # prepare the output file
 process.TFileService = cms.Service('TFileService',
