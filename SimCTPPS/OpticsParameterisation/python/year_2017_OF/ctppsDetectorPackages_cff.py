41205a0818adbbd0464803c1ec56307105f5b2e4
Jan Kaspar <jan.kaspar@cern.ch>
2018-11-20 14:50:44 +0100

    Commented out maxX fields.

--- SimCTPPS/OpticsParameterisation/python/year_2017_OF/ctppsDetectorPackages_cff.py
+++ SimCTPPS/OpticsParameterisation/python/year_2017_OF/ctppsDetectorPackages_cff.py
@@ -17,13 +17,13 @@ detectorPackages = cms.VPSet(
         potId = cms.uint32(0x76180000), # 003
         interpolatorName = cms.string('45-210-fr'),
         zPosition = cms.double(-212.551),
-        maxX = cms.double(15.5),
+        #maxX = cms.double(15.5),
     ),
     genericStripsPackage.clone(
         potId = cms.uint32(2023227392), # 023
         interpolatorName = cms.string('45-220-fr'),
         zPosition = cms.double(-219.550),
-        maxX = cms.double(15.5),
+        #maxX = cms.double(15.5),
     ),
 
     #----- sector 56
@@ -31,12 +31,12 @@ detectorPackages = cms.VPSet(
         potId = cms.uint32(0x77180000), # 103
         interpolatorName = cms.string('56-210-fr'),
         zPosition = cms.double(+212.551),
-        maxX = cms.double(14.),
+        #maxX = cms.double(14.),
     ),
     genericStripsPackage.clone(
         potId = cms.uint32(2040004608), # 123
         interpolatorName = cms.string('56-220-fr'),
         zPosition = cms.double(+219.550),
-        maxX = cms.double(14.),
+        #maxX = cms.double(14.),
     ),
 )
