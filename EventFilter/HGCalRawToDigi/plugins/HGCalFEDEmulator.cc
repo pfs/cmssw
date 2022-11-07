5c7d2f49c56f87a1d26a01e0bd0eea86ebf34822
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-11-07 14:40:45 +0100

    Added transient object storing channels PoI and H/T, E/B/O, and S bits info after emulation

--- EventFilter/HGCalRawToDigi/plugins/HGCalFEDEmulator.cc
+++ EventFilter/HGCalRawToDigi/plugins/HGCalFEDEmulator.cc
@@ -12,6 +12,7 @@
 #include "DataFormats/FEDRawData/interface/FEDRawDataCollection.h"
 
 #include "EventFilter/HGCalRawToDigi/interface/RawDataPackingTools.h"
+#include "EventFilter/HGCalRawToDigi/interface/HGCalFEDEmulatorInfo.h"
 
 #include "CLHEP/Random/RandFlat.h"
 #include "TChain.h"
@@ -26,15 +27,23 @@ private:
   void beginStream(edm::StreamID) override;
   void produce(edm::Event&, const edm::EventSetup&) override;
 
+  struct HeaderBits_t {
+    bool bitO, bitB, bitE, bitT, bitH, bitS;
+  };
+  std::vector<uint32_t> produceECONEvent(const edm::Event&, uint64_t&, HeaderBits_t&) const;
+
   const std::vector<std::string> input_files_;
+  const std::vector<unsigned int> enabled_channels_;
   const unsigned int num_channels_;
   const unsigned int header_marker_;
   const unsigned int idle_marker_;
   const unsigned int fed_id_;
   const double chan_surv_prob_;
+  const bool store_emul_info_;
 
   std::unique_ptr<TChain> chain_;
 
+  // input tree collections
   struct TreeEvent {
     int event, chip, half, channel, adc, adcm, toa, tot, totflag, bxcounter, eventcounter, orbitcounter;
   };
@@ -51,37 +60,88 @@ private:
 
   edm::Service<edm::RandomNumberGenerator> rng_;
   edm::EDPutTokenT<FEDRawDataCollection> fedRawToken_;
+  edm::EDPutTokenT<HGCalFEDEmulatorInfo> fedEmulInfoToken_;
 };
 
 HGCalFEDEmulator::HGCalFEDEmulator(const edm::ParameterSet& iConfig)
     : input_files_(iConfig.getParameter<std::vector<std::string>>("inputs")),
+      enabled_channels_(iConfig.getParameter<std::vector<unsigned int>>("enabledChannels")),
       num_channels_(iConfig.getParameter<unsigned int>("numChannels")),
       header_marker_(iConfig.getParameter<unsigned int>("headerMarker")),
       idle_marker_(iConfig.getParameter<unsigned int>("idleMarker")),
       fed_id_(iConfig.getParameter<unsigned int>("fedId")),
       chan_surv_prob_(iConfig.getParameter<double>("channelSurvProb")),
+      store_emul_info_(iConfig.getParameter<bool>("storeEmulatorInfo")),
       chain_(new TChain(iConfig.getParameter<std::string>("treeName").data())) {
-  fedRawToken_ = produces<FEDRawDataCollection>();
   if (!rng_.isAvailable())
     throw cms::Exception("HGCalFEDEmulator")
         << "The HGCalFEDEmulator module requires the RandomNumberGeneratorService,\n"
            "which appears to be absent. Please add that service to your configuration\n"
            "or remove the modules that require it.";
+
+  fedRawToken_ = produces<FEDRawDataCollection>();
+  if (store_emul_info_)
+    fedEmulInfoToken_ = produces<HGCalFEDEmulatorInfo>();
 }
 
 void HGCalFEDEmulator::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   if (it_data_ == data_.end())
     throw cms::Exception("HGCalFEDEmulator") << "Insufficient number of events were retrieved from input tree to "
                                                 "proceed with the generation of emulated events.";
+  //auto* rng_engine = &rng_->getEngine(iEvent.streamID());
+
+  uint64_t enabled_channels;
+  HeaderBits_t header_bits;
+  auto econ_event = produceECONEvent(iEvent, enabled_channels, header_bits);
+  size_t event_size = econ_event.size() * sizeof(econ_event.at(0));
+
+  // fill the output FED raw data collection
   FEDRawDataCollection raw_data;
+  auto& fed_data = raw_data.FEDData(fed_id_);
+
+  fed_data.resize(event_size);
+  memcpy(fed_data.data(), econ_event.data(), event_size);
+
+  iEvent.emplace(fedRawToken_, std::move(raw_data));
+
+  // store the emulation information if requested
+  if (store_emul_info_) {
+    HGCalFEDEmulatorInfo emul_info(enabled_channels,
+                                   header_bits.bitO,
+                                   header_bits.bitB,
+                                   header_bits.bitE,
+                                   header_bits.bitT,
+                                   header_bits.bitH,
+                                   header_bits.bitS);
+    iEvent.emplace(fedEmulInfoToken_, std::move(emul_info));
+  }
+  ++it_data_;
+}
+
+std::vector<uint32_t> HGCalFEDEmulator::produceECONEvent(const edm::Event& iEvent,
+                                                         uint64_t& enabled_channels,
+                                                         HeaderBits_t& header_bits) const {
+  enum HGCROCEventRecoStatus { PerfectReco = 0, GoodReco = 1, FailedReco = 2, AmbiguousReco = 3 };
+
+  //FIXME to be parameterised
+  uint8_t ht = HGCROCEventRecoStatus::PerfectReco, ebo = 0;
+  bool bitS = false;
+  uint32_t crc = 0;  //FIXME CRC is fake
+
   auto* rng_engine = &rng_->getEngine(iEvent.streamID());
   std::vector<uint32_t> econ_event;
+  enabled_channels = 0ul;  // reset the list of channels enabled
   for (const auto& jt : it_data_->second) {
     std::vector<bool> chmap(num_channels_, true);
-    for (size_t i = 0; i < chmap.size(); i++)
-      chmap[i] = CLHEP::RandFlat::shoot(rng_engine) < chan_surv_prob_;
+    for (size_t i = 0; i < chmap.size(); i++) {
+      // randomly choosing the channels to be shot at
+      chmap[i] = (!enabled_channels_.empty() &&
+                  std::find(enabled_channels_.begin(), enabled_channels_.end(), i) != enabled_channels_.end()) &&
+                 (CLHEP::RandFlat::shoot(rng_engine) < chan_surv_prob_);
+      enabled_channels |= (chmap[i] << i);
+    }
 
-    auto erxData = hgcal::econd::eRxSubPacketHeader(0, 0, 0, jt.second.cm0, jt.second.cm1, chmap);
+    auto erxData = hgcal::econd::eRxSubPacketHeader(0, 0, false, jt.second.cm0, jt.second.cm1, chmap);
     for (size_t i = 0; i < num_channels_; i++) {
       if (!chmap[i])
         continue;
@@ -101,35 +161,39 @@ void HGCalFEDEmulator::produce(edm::Event& iEvent, const edm::EventSetup& iSetup
 
     econ_event.insert(econ_event.end(), erxData.begin(), erxData.end());
   }
+
   auto econdH = hgcal::econd::eventPacketHeader(header_marker_,
                                                 econ_event.size() + 1,
                                                 true,
                                                 false,
-                                                0,
-                                                0,
+                                                // HGCROC Event reco status across all active eRxE-B-O:
+                                                ht,   // HDR/TRL numbers
+                                                ebo,  // Event/BX/Orbit numbers
                                                 false,
                                                 false,
                                                 0,
                                                 std::get<0>(it_data_->first),
                                                 std::get<1>(it_data_->first),
                                                 std::get<2>(it_data_->first),
-                                                false,
+                                                bitS,  // OR of "Stat" bits for all active eRx
                                                 0,
                                                 0);
   econ_event.insert(econ_event.begin(), econdH.begin(), econdH.end());
-  econ_event.push_back(0);  //CRC is fake
+  econ_event.push_back(crc);
   econ_event.push_back(idle_marker_);
 
-  size_t event_size = econ_event.size() * sizeof(econ_event.at(0));
+  //for (size_t i = 0; i < econ_event.size(); i++)
+  //  econ_event[i] = htobe32(econ_event[i]);
 
-  // fill the output FED raw data collection
-  auto& fed_data = raw_data.FEDData(fed_id_);
+  //FIXME check endianness of these two
+  header_bits.bitH = (ht >> 1) & 0x1;
+  header_bits.bitT = ht & 0x1;
+  header_bits.bitE = (ebo >> 2) & 0x1;
+  header_bits.bitB = (ebo >> 1) & 0x1;
+  header_bits.bitO = ebo & 0x1;
+  header_bits.bitS = bitS;
 
-  fed_data.resize(event_size);
-  memcpy(fed_data.data(), econ_event.data(), event_size);
-
-  iEvent.emplace(fedRawToken_, std::move(raw_data));
-  ++it_data_;
+  return econ_event;
 }
 
 void HGCalFEDEmulator::beginStream(edm::StreamID) {
@@ -173,18 +237,23 @@ void HGCalFEDEmulator::beginStream(edm::StreamID) {
       data_[key][erxKey].toa.push_back(event.toa);
     }
   }
+  //FIXME introduce (optional) capture block building
   it_data_ = data_.begin();
 }
 
 void HGCalFEDEmulator::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
   desc.add<std::string>("treeName", "unpacker_data/hgcroc");
-  desc.add<std::vector<std::string>>("inputs", {});
-  desc.add<unsigned int>("numChannels", 37);
+  desc.add<std::vector<std::string>>("inputs", {})
+      ->setComment("list of input files containing HGCROC emulated/test beam frames");
+  desc.add<std::vector<unsigned int>>("enabledChannels", {})->setComment("list of channels to be enabled in readout");
+  desc.add<unsigned int>("numChannels", 37)->setComment("number of channels managed in ECON-D");
   desc.add<unsigned int>("headerMarker", (0xaa000000 >> 23) & 0x1ff);
   desc.add<unsigned int>("idleMarker", 0x55550000);
-  desc.add<unsigned int>("fedId", 0);
+  desc.add<unsigned int>("fedId", 0)->setComment("FED number delivering the emulated frames");
   desc.add<double>("channelSurvProb", 999.);
+  desc.add<bool>("storeEmulatorInfo", true)
+      ->setComment("also append a 'truth' auxiliary info to the output event content");
   descriptions.add("hgcalEmulatedFEDRawData", desc);
 }
 
